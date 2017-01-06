84643e890a09d980e0a724e7df10e37bfbee3843
Vyacheslav Matyukhin <me@berekuk.ru>
2017-01-06 07:15:31 +0300

    SlackUpic profile link

--- front/components/SlackUpic.js
+++ front/components/SlackUpic.js
@@ -1,8 +1,11 @@
 import * as React from 'react';
 
+const SLACK_SERVER = 'lesswrongru';
 const SlackUpic = ({ user }) => (
     <div className="slack-upic">
-      <img src={user.profile.image_72} />
+      <a href={`https://${SLACK_SERVER}.slack.com/team/${user.name}`}>
+        <img src={user.profile.image_72} />
+      </a>
     </div>
 );
 
