ff13d5cf192cd9071d46d003caa2251b9648f236
Vyacheslav Matyukhin <me@berekuk.ru>
2017-02-25 03:29:38 +0300

    app.js: lintify

--- front/app.js
+++ front/app.js
@@ -1,44 +1,48 @@
-var express = require('express');
-var SearchkitExpress = require('searchkit-express');
-var bodyParser = require('body-parser');
-var path = require('path');
-var fs = require('fs');
+const express = require('express');
+const SearchkitExpress = require('searchkit-express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
 
-var app = express();
+const app = express();
 app.use(express.static('static'));
 
-var archiveDir = path.join(__dirname, '..', 'archive');
+const archiveDir = path.join(__dirname, '..', 'archive');
 
 app.use('/archive-data', express.static(archiveDir));
 
-app.get('/archive-data/:channel/dates', function(req, res) {
-  fs.readdir(
-    path.join(archiveDir, req.params.channel),
-    (err, files) => {
-      res.send(
-        files.map(function (filename) {
-            return filename.replace(RegExp('\.json$'), '');
-        })
-      );
-    }
-  );
-});
+app.get(
+  '/archive-data/:channel/dates',
+  (req, res) => {
+    fs.readdir(
+      path.join(archiveDir, req.params.channel),
+      (err, files) => {
+        res.send(
+          files.map(filename => filename.replace(RegExp('\.json$'), ''))
+        );
+      }
+    );
+  }
+);
 
-app.get('/*', function(req, res) {
-  res.sendFile(__dirname + '/static/index.html');
-});
+app.get(
+  '/*',
+  (req, res) => {
+    res.sendFile(`${__dirname}/static/index.html`);
+  }
+);
 
 app.use(bodyParser.json());
 SearchkitExpress({
-    host: process.env.ELASTIC_URL || 'http://localhost:9200',
-    index: 'slack',
-    queryProcessor: function(query, req, res) {
-        //do neccessery permissions, prefilters to query object
-        //then return it
-        return query;
-    }
+  host: process.env.ELASTIC_URL || 'http://localhost:9200',
+  index: 'slack',
+  queryProcessor: function(query, req, res) {
+    // do neccessery permissions, prefilters to query object
+    // then return it
+    return query;
+  },
 }, app);
 
-app.listen(8000, 'localhost', function () {
-    console.log('Slack search is listening!');
-})
+app.listen(8000, 'localhost', () => {
+  console.log('Slack search is listening!');
+});
