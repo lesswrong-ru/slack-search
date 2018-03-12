2a9c33d25a0f19bd4a2faf390859393019d6d2ba
Vyacheslav Matyukhin <me@berekuk.ru>
2018-03-13 01:54:19 +0300

    fix #15; prettier

--- front/src/components/SlackMessage.js
+++ front/src/components/SlackMessage.js
@@ -14,44 +14,41 @@ import { observer, inject } from 'mobx-react';
 const SLACK_SERVER = 'lesswrongru';
 
 const SpecialMention = ({ text }) => (
-  <span style={{fontWeight: 'bold', background: '#fff4bf'}}>
-    @{text}
-  </span>
+  <span style={{ fontWeight: 'bold', background: '#fff4bf' }}>@{text}</span>
 );
 
-const SlackMessage = inject("store")(observer(
-  class SlackMessage extends React.Component {
-    parseLink(link) {
-      const fallback = () => <code>{'<' + link + '>'}</code>;
+const SlackMessage = inject('store')(
+  observer(
+    class SlackMessage extends React.Component {
+      parseLink(link) {
+        const fallback = () => <code>{'<' + link + '>'}</code>;
 
-      if (link.substr(0, 2) === '@U') {
-        const user_id = link.substr(1);
-        const user = this.props.store.getUser(user_id);
-        return <SlackUserMention name={user.name} />;
-      }
-      else if (link.substr(0, 2) === '#C') {
-        const match = link.match(RegExp('#C.*?\\|(.*)'));
-        if (!match) {
-          return fallback(); // can't parse
+        if (link.substr(0, 2) === '@U') {
+          const user_id = link.substr(1);
+          const user = this.props.store.getUser(user_id);
+          return <SlackUserMention name={user.name} />;
+        } else if (link.substr(0, 2) === '#C') {
+          const match = link.match(RegExp('#C.*?\\|(.*)'));
+          if (!match) {
+            return fallback(); // can't parse
+          }
+          return <SlackChannelMention name={match[1]} />;
+        } else if (link[0] === '!') {
+          return <SpecialMention text={link.substr(1)} />;
+        } else if (link.substr(0, 2) === '@W') {
+          // slack documents that it should be replaced like @U, but I can't find any examples for @W
+          return fallback();
+        } else {
+          return <a href={link}>{link}</a>;
         }
-        return <SlackChannelMention name={match[1]} />;
       }
-      else if (link[0] === '!') {
-        return <SpecialMention text={link.substr(1)} />
-      }
-      else if (link.substr(0, 2) === '@W') { // slack documents that it should be replaced like @U, but I can't find any examples for @W
-        return fallback();
-      }
-      else {
-        return <a href={link}>{link}</a>;
-      }
-    }
 
-    renderText() {
-      const { message } = this.props;
-      let bits = message.text.split(/(<.*?>|\n)/);
-      bits = bits.map(
-        bit => {
+      renderText() {
+        const { message } = this.props;
+
+        let text = message.text || '';
+        let bits = text.split(/(<.*?>|\n)/);
+        bits = bits.map(bit => {
           if (bit === '\n') {
             return <br />;
           }
@@ -60,64 +57,64 @@ const SlackMessage = inject("store")(observer(
             return bit;
           }
           return this.parseLink(match[1]);
-        }
-      );
-      return bits;
+        });
+        return bits;
 
-      const text = message.text.replace(
-        /<(.*?)>/g,
-        (match, inner) => this.parseLink(inner)
-      );
-      return text;
-    }
-
-    render() {
-      const { store, message, collapse } = this.props;
-
-      const ts = moment(parseFloat(message.ts) * 1000);
-      const now = moment();
-      const date = (ts.year() === now.year() ? ts.format('MMM Do') : ts.format('YYYY MMM Do'));
-
-      const user = store.getUser(message.user);
-
-      let asideEl = (
-        <SlackUpic user={user} />
-      );
-      let timeEl = (
-        <div className="time">
-          <a href={`https://${SLACK_SERVER}.slack.com/archives/${message.channel}/p${message.ts.replace('.', '')}`}>
-            {ts.format('HH:mm')}
-          </a>
-        </div>
-      );
-      let headlineEl = (
-        <div className="slack-message__headline">
-          <SlackUser name={user.name} />
-          {timeEl}
-        </div>
-      );
-      if (collapse) {
-        asideEl = timeEl;
-        headlineEl = undefined;
+        text = text.replace(/<(.*?)>/g, (match, inner) =>
+          this.parseLink(inner)
+        );
+        return text;
       }
 
-      return (
-        <div className="slack-message">
-          <div className="slack-message__aside">
-            {asideEl}
+      render() {
+        const { store, message, collapse } = this.props;
+
+        const ts = moment(parseFloat(message.ts) * 1000);
+        const now = moment();
+        const date =
+          ts.year() === now.year()
+            ? ts.format('MMM Do')
+            : ts.format('YYYY MMM Do');
+
+        const user = store.getUser(message.user);
+
+        let asideEl = <SlackUpic user={user} />;
+        let timeEl = (
+          <div className="time">
+            <a
+              href={`https://${SLACK_SERVER}.slack.com/archives/${
+                message.channel
+              }/p${message.ts.replace('.', '')}`}
+            >
+              {ts.format('HH:mm')}
+            </a>
           </div>
+        );
+        let headlineEl = (
+          <div className="slack-message__headline">
+            <SlackUser name={user.name} />
+            {timeEl}
+          </div>
+        );
+        if (collapse) {
+          asideEl = timeEl;
+          headlineEl = undefined;
+        }
 
-          <div className="slack-message__main">
-            {headlineEl}
+        return (
+          <div className="slack-message">
+            <div className="slack-message__aside">{asideEl}</div>
 
-            <div className="slack-message-text">
-              {this.renderText()}
+            <div className="slack-message__main">
+              {headlineEl}
+
+              <div className="slack-message-text">{this.renderText()}</div>
             </div>
           </div>
-        </div>
-      );
+        );
+      }
     }
-  }
-));
+  )
+);
 
 export default SlackMessage;
