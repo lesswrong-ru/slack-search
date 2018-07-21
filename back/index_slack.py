8aff0866b35ff311e84a09e28902b641ef913464
Vyacheslav Matyukhin <me@berekuk.ru>
2018-07-22 02:18:17 +0300

    zero downtime indexing; channel and user are keywords

--- back/index_slack.py
+++ back/index_slack.py
@@ -13,14 +13,24 @@ class Elastic:
         self.endpoint = endpoint
         self._bulk_buffer = []
 
+    def exists_index(self, index):
+        code = requests.head(
+            f'{self.endpoint}/{index}'
+        ).status_code
+        if code == 404:
+            return False
+        if code == 200:
+            return True
+        raise Exception(f"Error while checking if {index} exists")
+
     def delete_index(self, index):
-        print(colored('Deleting index {}'.format(index), 'red'))
+        print(colored(f'Deleting index {index}', 'red'))
         requests.delete(
             '{}/{}'.format(self.endpoint, index)
         ).raise_for_status()
 
     def create_index(self, index):
-        print(colored('Creating index {}'.format(index), 'green'))
+        print(colored(f'Creating index {index}', 'green'))
         requests.put(
             '{}/{}'.format(self.endpoint, index),
             json={
@@ -31,6 +41,13 @@ class Elastic:
                                 'type': 'date',
                                 'format': 'epoch_millis',
                             },
+                            'channel': {
+                                'type': 'keyword',
+                            },
+                            'user': {
+                                'type': 'keyword',
+                            },
+                            # username is not a keyword (should this be changed?), because it can be a full name like "John Doe"
                             'attachments': {
                                 'properties': {
                                     'ts': {
@@ -42,6 +59,37 @@ class Elastic:
                     }
                 }
             }
+        ).raise_for_status()
+
+    def create_alias(self, alias, index):
+        print(colored('Creating alias {alias} for {index}', 'green'))
+        requests.put(
+            f'{self.endpoint}/{index}/_alias/{alias}'
+        ).raise_for_status()
+
+    def alias_targets(self, alias):
+        r = requests.get(
+            f'{self.endpoint}/*/_alias/{alias}'
+        )
+        r.raise_for_status()
+        data = r.json()
+        return list(data.keys())
+
+    def switch_alias(self, alias, index):
+        # the alias currently points to N old indices, let's remove them all
+        actions = [
+            {'remove': { 'index': index, 'alias': alias }}
+            for index in self.alias_targets(alias)
+        ]
+
+        actions.append({
+            'add': { 'index': index, 'alias': alias }
+        })
+
+        print(colored('Redirecting alias {alias} to {index}', 'green'))
+        requests.post(
+            f'{self.endpoint}/_aliases',
+            json={'actions': actions}
         ).raise_for_status()
 
     def add(self, index, doc_id, content):
@@ -93,14 +141,12 @@ class Elastic:
         self._bulk_buffer = []
 
 
-INDEX = 'slack.messages'
-
-def index_users(archive, elastic):
+def index_users(archive, elastic, index):
     for user in archive.users():
-        elastic.add(INDEX, user['id'], user)
+        elastic.add(index, user['id'], user)
 
 
-def index_messages(archive, elastic, min_date=None, max_date=None):
+def index_messages(archive, elastic, index, min_date=None, max_date=None):
     users = archive.users_dict()
     users_banned_from_index = set()
     for message in archive.traverse(min_date=min_date, max_date=max_date):
@@ -112,7 +158,7 @@ def index_messages(archive, elastic, min_date=None, max_date=None):
         message['millits'] = int(float(message['ts']) * 1000) # milliseconds since epoch
 
         elastic.bulk_push(
-            INDEX,
+            index,
             doc_id,
             message
         )
@@ -128,6 +174,9 @@ def main():
 
     args = parser.parse_args()
 
+    INDEX_SLOTS = [f'slack.messages.{i}' for i in range(1,3)]
+    ALIAS = 'slack.messages'
+
     def parse_if_defined(maybe_date_str):
         return datetime.strptime(maybe_date_str, '%Y-%m-%d').date() if maybe_date_str else None
     min_date = parse_if_defined(args.min_date)
@@ -136,13 +185,21 @@ def main():
     archive = SlackArchive(args.root)
     elastic = Elastic()
 
-    if args.mode == 'reindex':
-        elastic.delete_index(INDEX)
-        elastic.create_index(INDEX)
-    elif args.mode == 'initial':
-        elastic.create_index(INDEX)
+    if args.mode == 'initial':
+        index = INDEX_SLOTS[0]
+        elastic.create_alias(ALIAS, index)
+    elif args.mode == 'reindex':
+        used_indices = elastic.alias_targets()
+        index = next(slot for slot in INDEX_SLOTS if slot not in used_indices)
+        print(colored(f'{",".join(used_indices)} is used, indexing to {index}'), 'green')
 
-    #index_users(archive, elastic)
-    index_messages(archive, elastic, min_date=min_date, max_date=max_date)
+    if elastic.exists_index(index):
+        elastic.delete_index(index)
+
+    elastic.create_index(index)
+
+    index_messages(archive, elastic, index, min_date=min_date, max_date=max_date)
+
+    elastic.switch_alias(alias, index)
 
 main()
