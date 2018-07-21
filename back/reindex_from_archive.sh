eeb30c5d71ce7709f3044f724bab3972460aaf4e
Vyacheslav Matyukhin <me@berekuk.ru>
2018-07-21 21:33:36 +0300

    upload_and_reindex supports "initial" option

--- back/reindex_from_archive.sh
+++ back/reindex_from_archive.sh
@@ -17,7 +17,8 @@ ROOT_DIR=$(absname $(dirname "$0")/..)
 
 cd $ROOT_DIR
 
+rm -rf archive-new
 unzip "$ZIP" -d archive-new
 . ./back/venv/bin/activate
-./back/index_slack.py archive-new
+./back/index_slack.py archive-new ${2:-reindex}
 rm -rf archive-old && mv archive archive-old && mv archive-new archive
--- back/upload_and_reindex.sh
+++ back/upload_and_reindex.sh
@@ -5,4 +5,4 @@ LOCAL_ZIP="$1"
 HOST=slack.lesswrong.ru
 DEPLOY_DIR=/srv/slack-search
 scp "$1" $HOST:$DEPLOY_DIR/archive-new.zip
-ssh $HOST "cd $DEPLOY_DIR && ./back/reindex_from_archive.sh ./archive-new.zip"
+ssh $HOST "cd $DEPLOY_DIR && ./back/reindex_from_archive.sh ./archive-new.zip ${2:-reindex}"
