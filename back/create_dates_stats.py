e52cfe2ce3efd3a794bbe6e6ccf9be6e42780548
Mikhail Dektyarev <mihail.dektyarow@gmail.com>
2019-05-23 00:17:46 +0300

    script to find days with no useful messages

---
+++ back/create_dates_stats.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import glob
+import os
+
+try:
+    from tqdm import tqdm
+except ImportError:
+    def tqdm(x):
+        return x
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("archive_dir")
+
+    args = parser.parse_args()
+
+    dirs = glob.glob(os.path.join(args.archive_dir, '*', ''))
+    for d in tqdm(dirs):
+        files = []
+        for f in glob.glob(os.path.join(d, '[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9].json')):
+            j = json.load(open(f))
+            has_messages = False
+            for item in j:
+                if item.get('subtype', None) in ['channel_join', 'channel_left']:
+                    has_messages = True
+                    break
+            files.append({
+                'date': os.path.basename(f).split('.')[0],
+                'has-messages': has_messages
+            })
+        json.dump(files, open(os.path.join(d, 'dates.json'), 'w'))
+
+
+if __name__ == "__main__":
+    main()
